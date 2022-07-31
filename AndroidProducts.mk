6a262b79b4566af1023f924376c021894dbb4be8
delano-git <noronha.delano@gmail.com>
2022-07-31 21:20:23 +0400

    lemonade: Initialize YAAP

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021 The YAAP Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_lemonade.mk
+    $(LOCAL_DIR)/yaap_lemonade.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_lemonade-user \
-    lineage_lemonade-userdebug \
-    lineage_lemonade-eng
+    yaap_lemonade-user \
+    yaap_lemonade-userdebug \
+    yaap_lemonade-eng
